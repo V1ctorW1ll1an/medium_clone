64f705432910cf2fb45b0c296a6b658328ead76f
Victor Willian de Sousa <victorwillian1012@gmail.com>
2021-01-27 00:50:51 -0300

    Add home module on store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,9 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { home } from '@/modules/home_page/home_store/index'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+  modules: { home }
 })
