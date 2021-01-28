1529c900242b5d93dc0ffd04b5704c4dcc2eda1d
Victor Willian de Sousa <victorwillian1012@gmail.com>
2021-01-28 13:07:55 -0300

    Creating HomeHeader component

---
+++ src/modules/home_page/components/HomeHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app-bar color="#ffffff">
+    <h1>
+      Header works
+    </h1>
+  </v-app-bar>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class HomeHeader extends Vue {}
+</script>
+
+<style scoped>
+</style>
