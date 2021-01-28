67e7ddf270046e6fe0d4a8a43ac79ee6f1b4ada9
Victor Willian de Sousa <victorwillian1012@gmail.com>
2021-01-28 13:08:11 -0300

    Creating Home layout

---
+++ src/modules/home_page/layout/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <HomeHeader />
+  </div>
+</template>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import HomeHeader from '@/modules/home_page/components/HomeHeader.vue'
+
+@Component({
+  components: {
+    HomeHeader
+  }
+})
+export default class Home extends Vue {}
+</script>
+<style lang="scss">
+</style>
