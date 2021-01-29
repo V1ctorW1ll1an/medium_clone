77092384769d1d1a2816eb543d44e6d51be1e7ce
Victor Willian de Sousa <victorwillian1012@gmail.com>
2021-01-28 22:10:10 -0300

    Creating home header

--- src/modules/home_page/components/HomeHeader.vue
+++ src/modules/home_page/components/HomeHeader.vue
@@ -1,17 +1,47 @@
 <template>
-  <v-app-bar color="#ffffff">
-    <h1>
-      Header works
-    </h1>
-  </v-app-bar>
+  <header class="home__header
+   header--background
+   w-100
+   d-flex
+   justify-center
+   align-center">
+    <v-app-bar
+      color="transparent"
+      flat
+      class="default-max-width
+       p-0"
+      height="76"
+    >
+      <Logo />
+      <NavBar />
+    </v-app-bar>
+  </header>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
+import Logo from '@/shared/Logo.vue'
+import NavBar from '@/modules/home_page/components/HomeNavBar.vue'
 
-@Component
+@Component({
+  components: {
+    Logo,
+    NavBar
+  }
+})
 export default class HomeHeader extends Vue {}
 </script>
 
-<style scoped>
+<style lang="scss" >
+.home__header {
+  &.header--background {
+    background-color: #ffc017;
+  }
+
+  .v-toolbar__content {
+    padding: 0 !important;
+    display: flex !important;
+    justify-content: space-between;
+  }
+}
 </style>
