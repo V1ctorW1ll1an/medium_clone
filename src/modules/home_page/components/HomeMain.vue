4ecc6af874d83d06e32ef201f08d239bad10db92
Victor Willian de Sousa <victorwillian1012@gmail.com>
2021-01-31 23:04:05 -0300

    Creating Home main component

---
+++ src/modules/home_page/components/HomeMain.vue
@@ -0,0 +1,46 @@
+<template>
+  <main class="home__main">hello</main>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import { Mutation } from 'vuex-class'
+@Component
+export default class HomeMain extends Vue {
+  @Mutation('setHeaderBg', { namespace: 'home' }) setHeaderBg: any
+  @Mutation('setHeaderBtnBg', { namespace: 'home' }) headerBtnBg: any
+
+  created() {
+    window.addEventListener('scroll', this.changeBg)
+  }
+
+  destroyed() {
+    window.removeEventListener('scroll', this.changeBg)
+  }
+
+  changeBg() {
+    const distance = this.getPosition()
+    if (distance > 60) {
+      this.setHeaderBg('#ffc017')
+      this.headerBtnBg('#000000')
+    } else {
+      this.setHeaderBg('#ffffff')
+      this.headerBtnBg('#1A8917')
+    }
+  }
+
+  getPosition() {
+    const element = document.querySelector('.home__main') as HTMLElement
+
+    const distance = element.getBoundingClientRect()
+
+    return distance.top
+  }
+}
+</script>
+
+<style lang="scss">
+.home__main {
+  height: 1500px;
+}
+</style>
--- src/modules/home_page/layout/Home.vue
+++ src/modules/home_page/layout/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
     <HomeHeader />
+    <HomeMain />
   </div>
 </template>
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 import HomeHeader from '@/modules/home_page/components/HomeHeader.vue'
+import HomeMain from '@/modules/home_page/components/HomeMain.vue'
 
 @Component({
   components: {
-    HomeHeader
+    HomeHeader,
+    HomeMain
   }
 })
 export default class Home extends Vue {}
